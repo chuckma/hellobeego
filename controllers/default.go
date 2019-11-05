1af1170ec03418ed7fe8bbba11059b3e812bc4c2
chuckma <robbincen@163.com>
2019-11-05 16:38:11 +0800

    init

---
+++ controllers/default.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"hellobeego/models"
+	_ "hellobeego/models"
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+	m:=models.GetPage()
+	c.Data["Website"] = m.Website
+	c.Data["Email"] = m.Email
+	c.TplName = "index.tpl"
+}
---
+++ models/page.go
@@ -0,0 +1,34 @@
+package models
+
+import(
+	"fmt"
+	"github.com/astaxie/beego/orm"
+	_"github.com/go-sql-driver/mysql"
+)
+
+
+type Page struct {
+	Id int
+	Website string
+	Email string
+}
+
+func init()  {
+	// 注册数据库
+	orm.RegisterDataBase("default","mysql",
+		"root@123456@tcp(127.0.0.1:3306)/test?charset=utf8")
+	orm.RegisterModel(new(Page))
+}
+func GetPage() Page {
+	//rtn:=Page{Website: "hellobeego.com", Email: "model@beego.com"}
+	//return rtn
+
+	o:=orm.NewOrm()
+  	//p:=Page{1}
+  	p:=Page{Id:1}
+	err:=o.Read(&p)
+	fmt.Println(err)
+	return p
+
+
+}

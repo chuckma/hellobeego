b7c867c3a3bb90612c6686b054cf96127548b2c6
chuckma <robbincen@163.com>
2019-11-05 17:27:03 +0800

    init

---
+++ models/init.go
@@ -0,0 +1,10 @@
+package models
+
+import (
+	//"github.com/astaxie/beego/orm"
+)
+func init() {
+	//orm.RegisterDataBase("default","mysql",
+	//	"root@123456@tcp(127.0.0.1:3306)/test?charset=utf8")
+	//orm.RegisterModel(new (MenuModel))
+}
